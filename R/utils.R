8d6c4b38dbe33718acdad887bde870782472abb5
aedobbyn <adobbs11@gmail.com>
2018-06-13 11:49:51 -0500

    try to make attributes sticky

--- R/utils.R
+++ R/utils.R
@@ -69,12 +69,55 @@ clean_data <- function(dat, o_zip) {
 }
 
 
+get_zones <- function(inp, verbose = TRUE, ...) {
+
+  if (verbose) {
+    message(glue::glue("Grabbing origin ZIP {inp}"))
+  }
+
+  this_url <- stringr::str_c(base_url, inp, collapse = "")
+  out <- get_data(this_url)
+
+  if (out$PageError == "No Zones found for the entered ZIP Code.") {
+    out <- tibble::tibble(
+      origin_zip = inp,
+      dest_zip_start = NA_character_,
+      dest_zip_end = NA_character_,
+      zone = NA_character_,
+      modifier_1 = NA_character_,
+      modifier_2 = NA_character_
+    )
+    out %<>% sticky::sticky()
+    attributes(out)$validity <- "invalid"
+
+    message(glue::glue("Origin zip {inp} is not in use."))
+
+  } else {
+    suppressWarnings({
+      out <- get_data(this_url) %>%
+        clean_data(o_zip = inp)
+      out %<>% sticky::sticky()
+
+      attributes(out)$validity <- "valid"
+
+      if (verbose) {
+        message(glue::glue("Recieved {as.numeric(max(out$dest_zip_end)) - as.numeric(min(out$dest_zip_start))} destination ZIPs for {as.numeric(max(out$zone)) - as.numeric(min(out$zone))} zones."))
+      }
+    })
+  }
+
+  return(out)
+}
+
+
 interpolate_zips <- function(df) {
   if ((attributes(df)$validity == "invalid")) {
     out <-
       df %>%
       dplyr::mutate(dest_zip = NA_character_) %>%
       dplyr::select(origin_zip, dest_zip, zone)
+
+    # attributes(df)$validity <- "invalid"
     return(out)
   }
 
@@ -95,41 +138,12 @@ interpolate_zips <- function(df) {
 }
 
 
-get_zones <- function(inp, verbose = TRUE, ...) {
 
-  if (verbose) {
-    message(glue::glue("Grabbing origin ZIP {inp}"))
-  }
-
-  this_url <- stringr::str_c(base_url, inp, collapse = "")
-  out <- get_data(this_url)
-
-  if (out$PageError == "No Zones found for the entered ZIP Code.") {
-    out <- tibble::tibble(
-      origin_zip = inp,
-      dest_zip_start = NA_character_,
-      dest_zip_end = NA_character_,
-      zone = NA_character_,
-      modifier_1 = NA_character_,
-      modifier_2 = NA_character_
-    )
-    attributes(out)$validity <- "invalid"
-
-    message(glue::glue("Origin zip {inp} is not in use."))
-
-  } else {
-    suppressWarnings({
-      out <- get_data(this_url) %>%
-        clean_data(o_zip = inp)
-
-      attributes(out)$validity <- "valid"
-
-      if (verbose) {
-        message(glue::glue("Recieved {as.numeric(max(out$dest_zip_end)) - as.numeric(min(out$dest_zip_start))} destination ZIPs for {as.numeric(max(out$zone)) - as.numeric(min(out$zone))} zones."))
-      }
-    })
-  }
-
-  return(out)
-}
-
+#' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
--- README.md
+++ README.md
@@ -5,10 +5,10 @@ A tidy interface to the USPS API.
 
 ### Installation Instructions
 
-`if (!require("devtools"))
-install.packages("devtools")`
-
-`devtools::install_github('aedobbyn/usps')`
+``` r
+if (!require("devtools")) install.packages("devtools")
+devtools::install_github('aedobbyn/usps')
+```
 
 <br>
 
@@ -23,7 +23,10 @@ install.packages("devtools")`
 ``` r
 library(usps)
 
-purrr::map_dfr(c(1, "007", 123), grab_zone_from_origin)
+origin_zips <- c(1, "007", 123)
+
+origin_zips %>% 
+  purrr::map_dfr(grab_zone_from_origin)
 #> Grabbing origin ZIP 001
 #> Origin zip 001 is not in use.
 #> Grabbing origin ZIP 007
@@ -44,4 +47,46 @@ purrr::map_dfr(c(1, "007", 123), grab_zone_from_origin)
 #>  9 007        012      7    
 #> 10 007        013      7    
 #> # ... with 1,851 more rows
+```
+
+<br> <br>
+
+The USPS web interface defaults to destination zip code ranges:
+
+<p align="center">
+
+<img src="./img/post_calc.jpg" alt="post_calc">
+
+</p>
+
+<br>
+
+which you can ask for by setting `as_range`. Instead of a `dest_zip`
+column, you’ll get a marker of the beginning of and end of the range
+range in `dest_zip_start` and `dest_zip_end`.
+
+<br>
+
+You can optionally display the `*` and `+` modifiers.
+
+``` r
+grab_zone_from_origin(42, 
+                      as_range = TRUE, 
+                      show_modifiers = TRUE)
+#> Grabbing origin ZIP 042
+#> Recieved 994 destination ZIPs for 8 zones.
+#> # A tibble: 127 x 6
+#>    origin_zip dest_zip_start dest_zip_end zone  modifier_1 modifier_2
+#>  * <chr>      <chr>          <chr>        <chr> <chr>      <chr>     
+#>  1 042        005            005          3     <NA>       <NA>      
+#>  2 042        006            009          7     <NA>       <NA>      
+#>  3 042        010            012          3     *          <NA>      
+#>  4 042        013            038          2     *          <NA>      
+#>  5 042        039            043          1     *          <NA>      
+#>  6 042        044            044          2     *          <NA>      
+#>  7 042        045            045          1     *          <NA>      
+#>  8 042        046            047          2     *          <NA>      
+#>  9 042        048            048          1     *          <NA>      
+#> 10 042        049            059          2     *          <NA>      
+#> # ... with 117 more rows
 ```
