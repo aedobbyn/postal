c166688260336359c641e4530e765bddf7d0d46e
aedobbyn <adobbs11@gmail.com>
2018-06-19 17:01:56 -0500

    get map zone plotting working

--- tests/testthat/test_fetch.R
+++ tests/testthat/test_fetch.R
@@ -1,22 +1,6 @@
 testthat::context("Test fetching")
 
 
-testthat::test_that("Safely getting data works", {
-  testthat::expect_null(try_get_data("foo") %>%
-                           purrr::pluck("result"))
-  testthat::expect_null(try_get_data(glue::glue("{three_digit_base_url}{'007'}")) %>%
-                          purrr::pluck("error"))
-  testthat::expect_null(try_get_data(
-    glue::glue("{five_digit_base_url}?origin={'06840'}&destination={'68007'}")) %>%
-                          purrr::pluck("error"))
-})
-
-
-testthat::test_that("Zips are prepped correctly", {
-  testthat::expect_warning(testthat::expect_equal(prep_zip("123456"), "12345"))
-})
-
-
 testthat::test_that("fetch_zones()", {
   testthat::expect_error(fetch_zones("foo123"))
 
@@ -42,24 +26,7 @@ testthat::test_that("fetch_zones()", {
 })
 
 
-testthat::test_that("Assignment of validity", {
-  testthat::expect_message(fetch_zones("1"), "Origin zip 001 is not in use.")
-  testthat::expect_equal("valid",
-                         get_zones("112") %>%
-                           dplyr::pull(validity) %>%
-                           dplyr::first())
-})
-
-
-testthat::test_that("Interpolation of zips in between ranges", {
-  testthat::expect_equal(2422,
-                         get_zones("123") %>%
-                           interpolate_zips() %>%
-                           nrow())
-})
-
-
-testthat::test_that("Priority Mail exceptions are noted", {
+testthat::test_that("Five digit fetch", {
   testthat::expect_equal(1, fetch_five_digit("60647", "11238") %>% nrow())
   testthat::expect_error(fetch_five_digit("86753", "11238"))
   testthat::expect_error(fetch_five_digit("123", "456"))
@@ -96,3 +63,6 @@ testthat::test_that("We can grab all origins", {
     "data.frame"
   )
 })
+
+
+
